c397099c0d39f9ba0b7e24eacc31cb9e3dbceb50
Yunus SEVĞANE <yunus@reklamatolyesi.org>
2021-11-03 21:52:50 +0300

    v1

--- src/sevgane.js
+++ src/sevgane.js
@@ -1,1 +1,13 @@
-ok;
+function http(url) {
+	var req = new XMLHttpRequest();
+	req.overrideMimeType('application/json');
+	req.open('GET', url, true);
+	req.onload = function() {
+		if (req.responseText.length > 2) {
+		}
+		else {
+			return req.responseText;
+		}
+	};
+	req.send(null);
+}
